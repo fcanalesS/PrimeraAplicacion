da67516343e5b4f564fb42baad455aba5aacbf76
Felipe Canales <fcanales@integraasesores.cl>
2024-04-13 18:21:55 -0400

    mi primera aplicación

---
+++ src/main/java/com/iplacex/PrimeraAplicacion/App.java
@@ -0,0 +1,13 @@
+package com.iplacex.PrimeraAplicacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, desde Eclipse en un proyecto MAVEN" );
+    }
+}
