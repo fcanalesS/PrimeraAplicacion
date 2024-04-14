91f89e4ab7c4e83855cfe7b91901ef7e05ec176b
Felipe Canales <fcanales@integraasesores.cl>
2024-04-13 22:33:41 -0400

    mensaje

--- src/main/java/com/iplacex/PrimeraAplicacion/App.java
+++ src/main/java/com/iplacex/PrimeraAplicacion/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!, desde Eclipse en un proyecto MAVEN" );
+        System.out.println( "Hello World!, desde Eclipse en un proyecto MAVEN, ahora descargado desde GitHub" );
     }
 }
